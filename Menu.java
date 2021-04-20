e42e429ba7074190bb8e9a9c6cbf1f30b4035eae
StivenCardonaVarelas <66332728+StivenCardonaVarelas@users.noreply.github.com>
2021-04-19 19:45:13 -0500

    Menú
    Donde los usuarios deciden que materia tomar mediante al codigo de la materia ....faltan variables que se encuentran en el metodo

---
+++ Menu.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajofinal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author stive
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner lector = new Scanner(System.in);
+        int salir = 0;
+        int materia = 0;
+        System.out.println("------Menú------");
+        System.out.println("------Codigo:" + "-------");
+        System.out.println("-------Materia:" + "------");
+        System.out.println("");
+        System.out.println("------Codigo:" + "-------");
+        System.out.println("-------Materia:" + "------");
+        System.out.println("");
+        System.out.println("------Codigo:" + "-------");
+        System.out.println("-------Materia:" + "------");
+        System.out.println("");
+        System.out.println("------Para salir precione 1----");
+        System.out.println("------Para elejir precione 2----");
+        salir = lector.nextInt();
+        if (salir == 1) {
+            System.out.println("El programa se a cerrado exitosamente");
+
+        } else {
+            System.out.println("Ingrese el codigo de la materia a presentar");
+            materia = lector.nextInt();
+            if (materia == 1) {
+
+                System.out.println(materia + " La materia elejida es:");
+            } else if (materia == 2) {
+                System.out.println(materia + " La materia elejida es:");
+
+            } else if (materia == 3) {
+
+                System.out.println(materia + " La materia elejida es:");
+            } else{
+        System.out.println("Materia no encontrada porfavor reiniciar el programa");
+                }
+    }
+
+    }
+}
